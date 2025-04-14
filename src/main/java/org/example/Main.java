9cb459a2069e8a9f8659e6eba6b146ac4a5fb46a
shahzain <shahzain@iconsult.com.pk>
2025-04-14 12:22:58 +0500

    Main File

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("New Hello");
+        System.out.println(".....");
+
+        System.out.println("....");
+        System.out.println("1212");
+        System.out.println("45445" +
+                "");
+
+    }
+}
